e7220e0d36bfdd68586637c08f89f1f2a4018759
Deva Midhun <devamidhun.3c.kmbvm@gmail.com>
2023-06-25 08:37:21 +0000

    grg

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -60,7 +60,7 @@ const config = {
     limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-PDPMCZ577P', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
