ce657362a5b47c6c4b130d06d3c67382d6b71929
Deva Midhun <devamidhun.3c.kmbvm@gmail.com>
2023-06-25 09:14:53 +0000

    fddgfh

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -82,7 +82,7 @@ const config = {
     hideAvatarRing: false,
 
     // Available themes. To remove any theme, exclude from here.
-    themes: [],
+    themes: ['dark'],
 
     // Custom theme
     customTheme: {
